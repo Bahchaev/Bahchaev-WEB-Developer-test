ea19c96394ccb21b960aec2312cea9e3ab44c3d0
Bahchaev <wbahas@mail.ru>
2020-01-23 23:07:22 +0700

    new component "ChartDraw"

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import SetPlayersNumber from "../SetPlayersNumber";
+
 import ChartDraw from "../ChartDraw";
-// import ShowList from "../ShowList";
-// import FractionList from "../../fractionList";
-// import FractionConfig from "../FractionConfig";
+
 
 function App() {
     return (
         <>
-            {/*<ShowList text={"Список фракций:"} listName={Array.from(FractionList.keys())}/>*/}
-            {/*<SetPlayersNumber/>*/}
-            {/*<p></p>*/}
             <ChartDraw/>
-
         </>
     )
 }
